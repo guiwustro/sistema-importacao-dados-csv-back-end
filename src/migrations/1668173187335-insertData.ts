cd25889368ffe1a8a47c96d39a4ead3e44ead0d5
Guilherme Wustro <gui.wustro@gmail.com>
2022-11-11 10:51:15 -0300

    fix: add insert data

--- src/migrations/1668173187335-insertData.ts
+++ src/migrations/1668173187335-insertData.ts
@@ -3,127 +3,126 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class insertData1668173187335 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		`
-        
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Damian Faulkner","Jan 28, 2022","$92.33","pellentesque.tellus@google.net"),
-  ("Fulton Parrish","Jun 26, 2022","$23.51","nisl.quisque@yahoo.com"),
-  ("Lenore Ayala","Dec 23, 2022","$8.28","magnis.dis@protonmail.net"),
-  ("Abigail Bradford","Oct 9, 2022","$72.31","id.sapien@yahoo.ca"),
-  ("Tarik Donaldson","Apr 1, 2023","$92.38","auctor.ullamcorper.nisl@hotmail.couk"),
-  ("Josiah Camacho","Oct 6, 2022","$11.52","eros@yahoo.org"),
-  ("Ray Vance","Jan 1, 2022","$61.79","interdum.sed@yahoo.org"),
-  ("Kenyon Bonner","Sep 13, 2023","$0.74","orci.in@icloud.net"),
-  ("Martha Castillo","Jun 19, 2022","$82.87","morbi@icloud.net"),
-  ("Seth Trujillo","Aug 2, 2023","$76.30","mattis.ornare@yahoo.couk");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Winifred Mathis","Nov 16, 2022","$53.04","in.condimentum@protonmail.couk"),
-  ("Amena Ratliff","Aug 28, 2023","$81.35","accumsan.neque@aol.edu"),
-  ("Lana Joyner","Apr 10, 2023","$70.97","sed.turpis@hotmail.edu"),
-  ("Dana Jennings","Feb 15, 2022","$29.31","scelerisque.neque@yahoo.couk"),
-  ("Suki Delgado","Jul 29, 2022","$26.69","a@hotmail.ca"),
-  ("Grant Solomon","Feb 17, 2022","$17.74","libero.at@outlook.couk"),
-  ("Brian Kennedy","Mar 31, 2023","$93.40","curae.phasellus@aol.com"),
-  ("Perry Weiss","Oct 29, 2023","$99.25","amet.nulla@aol.com"),
-  ("May Wilkerson","Jan 10, 2023","$73.12","aliquet.phasellus@hotmail.edu"),
-  ("Nasim Williamson","Oct 29, 2023","$94.72","ac.mattis@outlook.net");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Lars Hebert","Apr 3, 2023","$27.58","lorem.ac@protonmail.net"),
-  ("Zorita Carney","Jul 17, 2023","$3.40","aliquam@icloud.ca"),
-  ("Steven Dunn","Jun 16, 2023","$60.78","luctus.felis@protonmail.ca"),
-  ("Clayton Emerson","Jan 16, 2023","$78.10","gravida.molestie@yahoo.net"),
-  ("Giselle Burks","Jul 23, 2023","$84.74","pede@protonmail.net"),
-  ("Bree Fuentes","Jun 3, 2023","$76.48","a@icloud.org"),
-  ("Malcolm Cohen","Jun 12, 2023","$47.01","gravida.molestie@yahoo.edu"),
-  ("Dylan Riddle","Mar 2, 2022","$91.27","mauris.eu@protonmail.com"),
-  ("Laura Bray","Nov 27, 2021","$47.98","enim@google.ca"),
-  ("Nevada Hodges","May 20, 2022","$44.18","non.vestibulum.nec@aol.couk");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Georgia Jordan","Dec 14, 2022","$9.74","molestie@protonmail.org"),
-  ("Carter Randall","Feb 16, 2022","$2.09","dictum.eu@icloud.com"),
-  ("Jolie Gibson","Feb 5, 2022","$91.56","pharetra.quisque@google.edu"),
-  ("Rogan Holman","Oct 1, 2023","$78.07","ut.semper.pretium@yahoo.com"),
-  ("Joseph Gardner","Dec 14, 2022","$76.24","ante.ipsum@hotmail.com"),
-  ("Libby Martin","Dec 20, 2021","$92.50","et.rutrum@protonmail.org"),
-  ("Fiona Greer","Aug 24, 2022","$58.49","metus.sit@hotmail.org"),
-  ("Jaime Guzman","Mar 4, 2023","$41.60","eleifend.non@outlook.couk"),
-  ("Athena Booker","Mar 25, 2023","$84.38","feugiat.placerat.velit@google.net"),
-  ("Zane Whitley","Mar 10, 2022","$55.18","consectetuer@google.couk");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Honorato Tate","Mar 15, 2023","$87.76","nunc.laoreet@hotmail.com"),
-  ("Suki Wilkins","May 5, 2023","$3.89","mauris.blandit.mattis@google.edu"),
-  ("Orlando Vinson","Sep 14, 2022","$78.50","adipiscing@hotmail.com"),
-  ("Karen Mcknight","Sep 18, 2023","$49.47","integer@yahoo.edu"),
-  ("Briar Gregory","Nov 9, 2023","$83.49","lorem.eu@google.com"),
-  ("Harlan Cain","Sep 11, 2022","$77.20","libero.integer@protonmail.edu"),
-  ("Harriet Patrick","Aug 11, 2022","$96.19","mauris@icloud.ca"),
-  ("Macy Herman","Feb 23, 2023","$83.92","sed@protonmail.net"),
-  ("Len Martin","Feb 10, 2022","$46.57","ullamcorper.eu.euismod@protonmail.ca"),
-  ("Giacomo Deleon","Nov 22, 2021","$19.87","lacinia@hotmail.org");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Dorian Brown","Dec 7, 2021","$29.68","amet.dapibus@google.com"),
-  ("Jillian Page","Apr 18, 2023","$94.43","dictum.mi.ac@google.net"),
-  ("Nigel Wilkerson","Jul 17, 2023","$76.96","eu.ultrices@icloud.ca"),
-  ("Jaquelyn Welch","Jan 5, 2023","$15.49","cum.sociis.natoque@icloud.ca"),
-  ("Hasad Crane","Nov 19, 2021","$92.23","sociosqu.ad@hotmail.edu"),
-  ("Jenette Medina","Apr 1, 2023","$8.36","tristique.senectus@protonmail.ca"),
-  ("Jael Bond","Feb 5, 2022","$47.47","et.risus.quisque@icloud.edu"),
-  ("Penelope Patrick","Jul 15, 2023","$82.15","phasellus@icloud.org"),
-  ("Brian Gentry","Feb 6, 2022","$75.82","a.mi@outlook.com"),
-  ("Hilary Mercer","Jan 25, 2022","$85.65","consectetuer.adipiscing@outlook.org");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Finn Joyce","Oct 5, 2022","$12.41","mi.aliquam.gravida@outlook.edu"),
-  ("Cleo Austin","Mar 27, 2022","$27.60","et.magnis.dis@google.edu"),
-  ("Brenna Moss","Aug 12, 2022","$8.81","velit@google.net"),
-  ("Chandler Holland","Apr 10, 2022","$46.47","eleifend.cras@protonmail.edu"),
-  ("Amity Patrick","Feb 23, 2023","$11.39","euismod.in@protonmail.net"),
-  ("Sonia Richardson","Oct 25, 2023","$90.38","quisque.imperdiet@outlook.net"),
-  ("Odette Decker","Jan 10, 2023","$74.55","tincidunt.adipiscing@outlook.com"),
-  ("Amy Pacheco","Jul 26, 2022","$81.90","blandit@aol.ca"),
-  ("Chester Vaughn","Nov 24, 2021","$13.65","ac.ipsum@protonmail.ca"),
-  ("Rudyard Montoya","Jun 10, 2023","$5.17","magna.duis@yahoo.org");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Caleb Barrera","Nov 2, 2023","$91.89","sed.nunc@hotmail.org"),
-  ("Yuri Zimmerman","Oct 2, 2023","$94.27","ipsum@icloud.net"),
-  ("Abdul Schwartz","Jul 19, 2022","$78.68","in.aliquet@yahoo.net"),
-  ("Evelyn Mueller","Dec 23, 2022","$92.36","sodales@icloud.com"),
-  ("Quentin Galloway","Aug 30, 2022","$51.41","auctor.velit.aliquam@protonmail.com"),
-  ("Diana Wheeler","Mar 11, 2022","$15.42","et@icloud.net"),
-  ("Cheryl Jensen","Nov 18, 2021","$6.11","proin.dolor.nulla@protonmail.edu"),
-  ("Aquila Suarez","May 16, 2022","$59.77","tempor.diam@icloud.ca"),
-  ("Zorita Perkins","Apr 21, 2022","$80.75","curabitur@hotmail.org"),
-  ("Channing Singleton","Jul 13, 2023","$60.77","diam.vel.arcu@outlook.couk");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Troy Meyer","Dec 2, 2022","$1.20","faucibus@protonmail.net"),
-  ("Hedy Lamb","Sep 25, 2022","$70.59","augue.porttitor.interdum@yahoo.org"),
-  ("Brett Alston","Jun 22, 2023","$44.53","in@protonmail.ca"),
-  ("Shellie Moses","Apr 8, 2023","$43.75","diam.nunc@hotmail.couk"),
-  ("Omar Wyatt","Nov 9, 2022","$31.27","nonummy@aol.ca"),
-  ("Julie Logan","Jan 16, 2023","$62.72","ornare.lectus@protonmail.net"),
-  ("Hollee Oneil","Oct 14, 2023","$3.86","aenean.massa@google.net"),
-  ("Kareem Wooten","Aug 31, 2023","$52.88","ridiculus.mus@aol.ca"),
-  ("Rahim Cook","Oct 19, 2022","$77.04","at.libero@hotmail.org"),
-  ("Quon Diaz","Feb 11, 2023","$12.27","ac.tellus.suspendisse@aol.com");
-INSERT INTO 'clients' ('name','birthDate','value','email')
-VALUES
-  ("Bethany Hopkins","Dec 18, 2021","$37.15","risus.duis@google.org"),
-  ("Dustin Sharp","Jan 26, 2023","$62.60","morbi.quis@google.couk"),
-  ("Karly Gillespie","Jun 10, 2023","$24.57","porttitor.eros.nec@aol.org"),
-  ("Emma Giles","Jun 19, 2022","$14.93","enim.etiam@google.com"),
-  ("Plato Rich","Mar 9, 2023","$95.45","sagittis.felis@icloud.org"),
-  ("Russell Wiley","Jun 27, 2022","$26.09","vulputate.mauris@icloud.com"),
-  ("Kylynn Dorsey","Oct 10, 2022","$59.28","ridiculus.mus.aenean@hotmail.couk"),
-  ("Leigh Burns","Mar 18, 2023","$20.79","diam.sed@aol.org"),
-  ("Imelda Keith","Sep 5, 2022","$76.80","porttitor.interdum@google.ca"),
-  ("Glenna Duran","Apr 10, 2023","$8.29","lobortis.tellus.justo@hotmail.edu");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Tarik Leach","2022-08-02 14:29:55","commodo.hendrerit@yahoo.couk","43573"),
+          ("Samuel Bray","2023-10-13 15:30:30","aliquam.eu@protonmail.couk","47459"),
+          ("Shana Powers","2023-08-19 19:29:29","adipiscing.lobortis@yahoo.net","93700"),
+          ("Uta Erickson","2021-12-19 16:20:31","facilisis.eget@icloud.org","93141"),
+          ("Eugenia Cortez","2022-10-02 13:07:36","orci.donec.nibh@outlook.net","16790"),
+          ("Lareina Alvarez","2022-02-25 14:23:56","sit.amet@icloud.ca","7720"),
+          ("Rose Landry","2022-09-11 23:59:27","tellus.lorem@hotmail.ca","13201"),
+          ("Chase Newton","2022-12-19 19:00:00","tincidunt.tempus.risus@outlook.net","47160"),
+          ("Lunea Harmon","2023-07-13 06:55:13","orci.ut.sagittis@aol.net","873"),
+          ("Patrick Odom","2023-07-15 13:02:07","molestie.tortor.nibh@google.edu","97906");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Kevin David","2022-04-24 20:16:39","luctus.ut@aol.couk","40154"),
+          ("Oscar Cotton","2022-09-30 14:01:03","vivamus.sit@aol.couk","28239"),
+          ("Alyssa Cherry","2023-07-28 02:49:04","ridiculus.mus@icloud.ca","65824"),
+          ("Rhiannon Roman","2022-04-06 21:22:10","elementum@hotmail.net","78746"),
+          ("Tucker Pacheco","2023-03-07 19:47:40","sed@yahoo.net","64992"),
+          ("Lysandra Hines","2023-07-02 20:49:46","habitant.morbi.tristique@icloud.ca","34628"),
+          ("Emma Vincent","2023-05-14 12:43:21","blandit.nam@icloud.ca","54323"),
+          ("Damian Burton","2022-01-31 09:16:01","molestie@google.net","83970"),
+          ("Andrew Hobbs","2021-11-26 04:57:06","lobortis.quam@aol.ca","44589"),
+          ("Avram Mayo","2022-08-16 06:11:44","vel.faucibus.id@outlook.net","19200");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Hamish Chen","2022-01-15 00:18:05","eget.magna@hotmail.ca","4991"),
+          ("Walker Erickson","2023-07-22 07:54:31","dignissim.pharetra.nam@google.edu","20709"),
+          ("Clark Hopper","2022-09-11 14:43:06","laoreet.posuere@google.ca","8991"),
+          ("Alea Phelps","2023-07-12 17:58:03","at.egestas.a@hotmail.ca","15306"),
+          ("Mikayla Frank","2022-02-22 09:26:26","cum.sociis@protonmail.com","62385"),
+          ("Belle Conrad","2023-07-11 20:26:18","purus.in@hotmail.edu","48825"),
+          ("Abel Ball","2021-11-21 03:52:06","adipiscing.lacus@google.org","87632"),
+          ("Octavia Morton","2022-03-15 04:24:06","dolor.dapibus.gravida@icloud.net","27995"),
+          ("Donna Mcleod","2023-10-02 03:29:34","eget.odio@protonmail.ca","93178"),
+          ("Mara ONeill","2022-08-19 18:13:36","nulla.cras.eu@outlook.net","30790");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Quintessa Collier","2022-10-23 15:22:20","parturient.montes.nascetur@google.com","92989"),
+          ("Tanner Spencer","2022-08-08 13:10:52","a.facilisis.non@icloud.couk","86049"),
+          ("Emerald Higgins","2023-10-04 22:36:37","urna.justo@outlook.ca","98008"),
+          ("Zahir Lyons","2023-01-24 17:16:42","vel.mauris@google.com","79667"),
+          ("Hilary Gilmore","2021-12-10 07:42:09","in.felis@yahoo.org","19969"),
+          ("Britanney Estrada","2022-06-19 18:04:34","sagittis@hotmail.net","94954"),
+          ("Tamara Keller","2022-05-08 08:13:20","ut@protonmail.com","84796"),
+          ("Shaine Oneal","2023-02-16 06:48:23","mi.aliquam@protonmail.ca","34686"),
+          ("Zeph Atkins","2023-08-08 19:37:12","malesuada.id@yahoo.edu","29907"),
+          ("Kevyn Juarez","2023-01-09 05:04:40","quis.massa.mauris@protonmail.ca","62797");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Mara Goodwin","2022-02-23 20:37:19","ut.molestie@hotmail.edu","45945"),
+          ("Abdul Romero","2022-03-07 08:36:55","tellus.faucibus.leo@yahoo.couk","53164"),
+          ("Shaine Salas","2023-11-09 00:22:04","commodo.hendrerit@protonmail.couk","53865"),
+          ("Christine Lee","2023-05-28 22:24:33","a.nunc.in@outlook.ca","44612"),
+          ("Murphy Maldonado","2022-08-22 21:40:51","vestibulum.ante@google.com","80756"),
+          ("Kaye Garza","2023-07-16 04:10:22","nonummy.ipsum.non@outlook.couk","12243"),
+          ("Wynne Lowe","2023-04-30 06:10:58","odio@hotmail.edu","9541"),
+          ("Rowan Randolph","2023-11-08 23:20:15","ac.fermentum@protonmail.ca","14331"),
+          ("Whoopi Henson","2022-04-24 21:21:44","mi.duis.risus@aol.net","64558"),
+          ("Heather Hobbs","2023-05-14 14:17:15","praesent.eu.dui@outlook.ca","30404");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Kevyn Berry","2022-04-20 22:35:34","luctus.et@outlook.couk","79121"),
+          ("Rinah Knox","2023-08-14 02:55:45","nonummy@protonmail.com","24642"),
+          ("Robert Hill","2023-08-16 08:41:57","sem.nulla@outlook.net","25322"),
+          ("Kieran Rutledge","2022-07-26 14:11:11","faucibus@aol.couk","3350"),
+          ("Ross Chambers","2023-08-21 07:54:57","mauris.rhoncus.id@aol.couk","29385"),
+          ("Ira Russo","2022-10-02 21:48:48","nullam@icloud.com","69952"),
+          ("Lucas Vang","2023-06-21 01:14:18","malesuada.vel@outlook.com","74041"),
+          ("Priscilla Cohen","2023-02-23 10:18:21","urna.nunc@hotmail.edu","14729"),
+          ("James Dale","2023-07-11 11:02:50","bibendum@aol.couk","98822"),
+          ("Curran Ray","2022-07-24 05:51:58","venenatis.vel@outlook.edu","68629");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Solomon Coffey","2023-08-30 00:33:35","cum@icloud.edu","61335"),
+          ("Ryder Miles","2022-10-18 08:42:13","ut.lacus@icloud.ca","29776"),
+          ("Fletcher Anderson","2022-11-29 02:15:01","laoreet.lectus.quis@aol.edu","50575"),
+          ("Kelly Swanson","2022-11-05 13:52:55","fringilla.porttitor@aol.edu","78949"),
+          ("Aurora Mckee","2022-07-25 06:00:23","mi@aol.ca","96147"),
+          ("Blythe Holden","2023-10-15 09:32:11","proin@protonmail.ca","1846"),
+          ("Belle Holder","2023-03-02 22:48:45","semper@yahoo.com","10739"),
+          ("Sharon Guy","2023-05-11 22:22:47","penatibus.et@hotmail.edu","38848"),
+          ("Maggie Thompson","2023-06-03 17:57:25","integer.mollis@google.org","28778"),
+          ("Jade Johnston","2022-11-12 19:24:20","pede@google.edu","14932");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Kennan Hendricks","2022-02-12 14:11:26","arcu.nunc@google.couk","5866"),
+          ("Garrett Hood","2022-03-31 05:49:23","mi.aliquam.gravida@aol.ca","53331"),
+          ("Graiden Mcpherson","2022-03-03 00:24:38","dapibus@icloud.net","55978"),
+          ("Yetta Thornton","2023-03-03 21:27:36","ac.sem@icloud.edu","9599"),
+          ("Katelyn Tillman","2022-07-26 05:58:15","fringilla.euismod@aol.ca","38758"),
+          ("Kareem Carter","2022-01-02 08:06:50","ipsum.phasellus@aol.com","35551"),
+          ("Leah Beard","2023-07-09 02:17:12","neque@icloud.com","7640"),
+          ("Sean Joyce","2022-02-04 11:46:45","libero.est@icloud.ca","40708"),
+          ("Zachary Mcdowell","2021-11-11 21:10:29","suspendisse.commodo.tincidunt@icloud.ca","42014"),
+          ("Daryl Alston","2023-04-10 19:28:01","lorem.semper@hotmail.ca","98025");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Aladdin Fuller","2023-08-19 12:33:12","et@google.couk","17887"),
+          ("Levi Carver","2022-08-09 10:08:44","phasellus.vitae@aol.org","12647"),
+          ("Jarrod Miranda","2022-01-06 22:04:31","ac.mi.eleifend@hotmail.net","97659"),
+          ("Reed Mccarthy","2023-05-11 11:44:34","donec.fringilla.donec@yahoo.ca","57125"),
+          ("Chelsea Holloway","2021-11-30 14:10:58","nec.orci.donec@yahoo.couk","289"),
+          ("Colby Frederick","2023-06-17 20:00:07","a.auctor@icloud.couk","3658"),
+          ("Gavin Mcgee","2023-02-03 13:40:45","hendrerit.id.ante@yahoo.edu","88226"),
+          ("Alexis Hays","2022-02-11 08:20:58","dui.suspendisse.ac@yahoo.net","80747"),
+          ("Rhea Pickett","2022-09-06 17:52:00","eros.non@icloud.com","25922"),
+          ("Angelica Rasmussen","2023-10-30 10:18:05","aliquet.libero@yahoo.couk","26142");
+        INSERT INTO clients (name,birthDate,email,value)
+        VALUES
+          ("Jasmine Massey","2022-02-17 02:07:45","urna.ut@protonmail.org","47366"),
+          ("Rhoda Diaz","2023-08-16 04:36:05","ridiculus@outlook.ca","54399"),
+          ("Kaseem Leon","2023-05-10 15:21:20","urna.ut@google.ca","68309"),
+          ("Ayanna Farley","2023-06-12 03:20:03","mus.proin.vel@icloud.com","36168"),
+          ("Lilah Lynn","2023-09-07 06:43:24","dolor.donec.fringilla@hotmail.com","82916"),
+          ("Emma Mcintyre","2022-10-11 16:41:42","vulputate.velit@aol.couk","92206"),
+          ("Wallace Griffin","2023-07-31 21:40:00","faucibus.lectus@google.org","19363"),
+          ("Kiayada Fischer","2023-07-28 11:10:48","auctor.non@aol.org","71371"),
+          ("Robert Morris","2022-07-20 07:55:10","nec.mollis@outlook.org","43486"),
+          ("Cheyenne Avery","2023-04-23 01:22:29","quis.tristique@icloud.ca","85353");
         `;
 	}
 
